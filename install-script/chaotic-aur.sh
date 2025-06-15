c9fac8e471a644ce38861f2a0beb6964abc9e6ab
tsuyoki2000 <84623270+tsuyoki2000@users.noreply.github.com>
2025-06-15 23:16:31 +0900

    Update chaotic-aur.sh

--- install-script/chaotic-aur.sh
+++ install-script/chaotic-aur.sh
@@ -9,8 +9,8 @@ sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 
 # Chaotic-AUR リポジトリのパッケージをダウンロードしてインストール
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' --noconfirm
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 
 # pacman.conf に Chaotic-AUR リポジトリを追加
 sudo echo -e "" >> /etc/pacman.conf
